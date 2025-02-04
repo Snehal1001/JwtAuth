6e3e3f518a631b7e14ee9aad5917d7e0ff638d30
Snehal Prajapati <snehal.prajapati.dev@gmail.com>
2025-02-04 18:20:55 +0530

    Add JWT Authentications

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,43 @@
+using System.Text;
+using API.Data;
+using API.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var JWTSettings = builder.Configuration.GetSection("JWTSetting");
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql("Data Source=auth.db"));
+builder.Services.AddIdentity<AppUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+
+builder.Services.AddAuthentication(opt =>
+{
+    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme; // Setes scheme authenticate schema
+    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme; // Sets scheme for Unauthorize [ how to respond when authentication fails (i.e., requesting a new JWT token or indicating that authentication is required)]
+    opt.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(opt =>
+{
+    opt.SaveToken = true;
+    opt.RequireHttpsMetadata = false; // means metadata and tokens can also work over HTTP. Generally we enforce HTTPS for better security
+    opt.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidAudience = JWTSettings["ValidAudience"],
+        ValidIssuer = JWTSettings["ValidIssuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(JWTSettings.GetSection("SecurityKey").Value!))
+    };
+});
+
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
