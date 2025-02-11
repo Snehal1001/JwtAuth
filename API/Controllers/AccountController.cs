4ad9e99e4384b44a7e51e8f2a50218d33e73f819
Snehal Prajapati <snehal.prajapati.dev@gmail.com>
2025-02-11 19:51:29 +0530

    API methods and RolesController Add

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,10 +1,17 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using API.Dtos;
 using API.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace API.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("api/[controller]")]
     public class AccountController : ControllerBase
@@ -24,7 +31,8 @@ namespace API.Controllers
         }
 
         // api/account/register
-        [HttpPost]
+        [AllowAnonymous]
+        [HttpPost("register")]
         public async Task<ActionResult<string>> Register(RegisterDto registerDto)
         {
             if (!ModelState.IsValid)
@@ -65,6 +73,138 @@ namespace API.Controllers
                 Message = "Account Created Successfully!"
             });
 
+        }
+
+        //api/account/login
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public async Task<ActionResult<AuthResponseDto>> LogIn(LoginDto loginDto)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+
+            if (user is null)
+            {
+                return Unauthorized(new AuthResponseDto
+                {
+                    IsSuccess = false,
+                    Message = "User not found with this email"
+                });
+            }
+
+            var result = await _userManager.CheckPasswordAsync(user, loginDto.Password);
+
+            if (!result)
+            {
+                return Unauthorized(new AuthResponseDto
+                {
+                    IsSuccess = false,
+                    Message = "Invalid Password"
+                });
+            }
+
+            var token = GenerateToken(user);
+
+            return Ok(new AuthResponseDto
+            {
+                Token = token,
+                IsSuccess = true,
+                Message = "Login Success"
+            });
+        }
+
+        private string GenerateToken(AppUser user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var key = Encoding.ASCII
+                              .GetBytes(_configuration.GetSection("JWTSetting:SecurityKey").Value!);
+
+            var roles = _userManager.GetRolesAsync(user).Result;
+
+            List<Claim> claims = [
+                new (JwtRegisteredClaimNames.Email, user.Email ?? ""),
+                new (JwtRegisteredClaimNames.Name, user.FullName ?? ""),
+                new (JwtRegisteredClaimNames.NameId, user.Id ?? ""),
+                new (JwtRegisteredClaimNames.Aud,
+                _configuration.GetSection("JWTSetting:ValidAudience").Value!),
+                new (JwtRegisteredClaimNames.Iss,
+                _configuration.GetSection("JWTSetting:ValidIssuer").Value!),
+            ];
+
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+
+            }
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddDays(1),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256
+                )
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+
+        //api/account/detail
+        [Authorize]
+        [HttpGet("detail")]
+        public async Task<ActionResult<UserDetailDto>> GetUserDetail()
+        {
+            var currentUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var user = await _userManager.FindByIdAsync(currentUserId!);
+
+            if (user is null)
+            {
+                return NotFound(new AuthResponseDto
+                {
+                    IsSuccess = false,
+                    Message = "User not found"
+                });
+            }
+
+            return Ok(new UserDetailDto
+            {
+                Id = user.Id,
+                Email = user.Email,
+                FullName = user.FullName,
+                Roles = _userManager.GetRolesAsync(user).Result.ToArray(),
+                PhoneNumber = user.PhoneNumber,
+                PhoneNumberConfirmed = user.PhoneNumberConfirmed,
+                AccessFailedCount = user.AccessFailedCount
+            });
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UserDetailDto>>> GetUsers()
+        {
+            var users = await _userManager.Users.ToListAsync();
+
+            var userDetails = new List<UserDetailDto>();
+
+            foreach (var user in users)
+            {
+                var roles = await _userManager.GetRolesAsync(user);
+
+                userDetails.Add(new UserDetailDto
+                {
+                    Id = user.Id,
+                    Email = user.Email,
+                    FullName = user.FullName,
+                    Roles = roles.ToArray()
+                });
+
+            }
+
+            return Ok(userDetails);
         }
     }
 }
